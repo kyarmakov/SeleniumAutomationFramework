00cd87fa8cd91fae49d652b2841954ec017bda5a
Kerim Yarmakov <kyarmakov@gmail.com>
2024-05-28 17:31:08 +0300

    add config.json

---
+++ src/main/java/constants/FrameworkConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public final class FrameworkConstants {
+    private static final String CONFIGJSONFILEPATH = System.getProperty("user.dir") + "src/test/resources/config/config.json";
+
+    private FrameworkConstants() {}
+
+    public static String getConfigJsonFilePath() {
+        return CONFIGJSONFILEPATH;
+    }
+}
---
+++ src/main/java/utils/JsonUtils.java
@@ -0,0 +1,37 @@
+package utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import constants.FrameworkConstants;
+import enums.ConfigProperties;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+public final class JsonUtils {
+    private static Map<String, String> CONFIGMAP;
+
+    private JsonUtils() {}
+
+    static {
+        try {
+            CONFIGMAP = new ObjectMapper()
+                    .readValue(new File(FrameworkConstants.getConfigJsonFilePath()), new TypeReference<>() {});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String get(ConfigProperties property) {
+        if (Objects.isNull(property) || Objects.isNull(CONFIGMAP.get(property.name().toLowerCase())))
+            try {
+                throw new Exception("Property " + property + " is not found. Please check config.json");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        return CONFIGMAP.get(property.name().toLowerCase()).trim();
+    }
+}
